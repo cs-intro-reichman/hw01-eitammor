64505cf55449d2fedd59a20bee2f623dfb21fa8f
eitammor <154350216+eitammor@users.noreply.github.com>
2023-12-22 11:46:19 +0200

    HW1
    Eitam Mor

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,11 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
+// The program add two given integers and prints the result.
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Set two variables according to the given numbers
+		int a = Integer.parseInt ( args[0] );
+		int b = Integer.parseInt ( args[1] );
+		//Prints the result of adding the two given numbers.
+		System.out.println ( a + " + " + b + " = " + (a+b));
 	}
 }
+		
--- Coins.java
+++ Coins.java
@@ -1,9 +1,14 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
+//The program gets a number of cents as a commandline argument and prints 
+//how to represent this quantity using as many quarters as possible plus
+//the remainder in cents
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		// Set one variable according to the amount of money.
+		int a = Integer.parseInt ( args[0] );
+		// Calculate the amount of quarters and cents 
+		int quarters = a/25;
+		int cents = a%25;
+		// Prints the result
+		System.out.println ( "Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
